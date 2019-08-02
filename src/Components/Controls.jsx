63e262827b8cb5ee6b988763a2183dae713c0b8e
ills5771 <ilyayelizarov1@gmail.com>
2019-08-02 10:18:23 -0700

    adds directions to controls

--- src/Components/Controls.jsx
+++ src/Components/Controls.jsx
@@ -1,15 +1,28 @@
-import React, { Component } from 'react';
-import './menu.css';
+import React, { Component } from "react";
+import "./menu.css";
 
 class Controls extends Component {
-  state = {  }
-  render() { 
-    return ( 
-      <div className="controls">
-        <h1>Controls</h1>
+  state = {};
+  render() {
+    return (
+      <div className='controls'>
+        <div
+          style={{
+            width: "150px",
+            display: "flex",
+            justifyContent: "space-between",
+            fontSize: "28px"
+          }}
+          className='controls-bar'
+        >
+          <h1>N</h1>
+          <h1>S</h1>
+          <h1>W</h1>
+          <h1>E</h1>
+        </div>
       </div>
-     );
+    );
   }
 }
- 
+
 export default Controls;
